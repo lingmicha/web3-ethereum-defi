cf7822a9cac97bbe29e0bae0bd98ececa6b2ae55
michael <michael@sun8am.com>
2022-10-20 23:04:35 +0800

    修改aave balance计算方式，同步更新示例和测试用例
    修改aave balance计算方式，同步更新示例和测试用例

--- tests/aave_v3/test_aave_balance.py
+++ tests/aave_v3/test_aave_balance.py
@@ -7,6 +7,13 @@ To run tests in this module:
     export BNB_CHAIN_JSON_RPC="https://bsc-dataseed.binance.org/"
     pytest -k test_token_tax
 
+    current block is 34,581,439
+    Large ADAI holder 229496.483194811632175501
+    Large stableDebtPolDAI holder 15161.687076820887075569
+    Large variableDebtPolEURS holder 21060.34
+    Large stableDebtPolEURS holder 20.1
+
+
 """
 import os
 
@@ -16,6 +23,7 @@ from eth_typing import HexAddress, HexStr
 from web3 import HTTPProvider, Web3
 
 from eth_defi.ganache import fork_network
+from eth_defi.aave_v3.constants import AaveToken, AaveNetwork, aave_v3_get_network_by_chain_id
 from eth_defi.aave_v3.balances import aave_v3_get_deposit_balance, aave_v3_get_variable_borrow_balance
 
 # https://docs.pytest.org/en/latest/how-to/skipping.html#skip-all-test-functions-of-a-class-or-module
@@ -26,25 +34,31 @@ pytestmark = pytest.mark.skipif(
 
 
 @pytest.fixture(scope="module")
-def aDai_holder() -> HexAddress:
+def aave_token_holder() -> HexAddress:
     """A random account picked from polygon that holds a lot of aDAI token.
 
     This account is unlocked on Ganache, so you have access to good aDAI stash.
 
-    `To find large holder accounts, use bscscan <https://polygonscan.com/address/0x29ad16353b0bebd3bbefee58d9f72887ebf4621e>`_.
+    `To find large holder accounts, use polygonscan <https://polygonscan.com/address/0x5b8effbdae5941b938c1a79616d185cc3c79d4ff>`_.
     """
     # Binance Hot Wallet 6
-    return HexAddress(HexStr("0x29ad16353b0bebd3bbefee58d9f72887ebf4621e"))
+    return Web3.toChecksumAddress("0x5b8effbdae5941b938c1a79616d185cc3c79d4ff")
+
+@pytest.fixture(scope="module")
+def aave_token() -> AaveToken:
+    aave_network = aave_v3_get_network_by_chain_id(137)
+    aave_token = aave_network.token_contracts['DAI']
+    return aave_token
 
 
 @pytest.fixture(scope="module")
-def ganache_polygon_chain_fork(aDai_holder) -> str:
+def ganache_polygon_chain_fork(aave_token_holder) -> str:
     """Create a testable fork of live Polygon chain.
 
     :return: JSON-RPC URL for Web3
     """
     mainnet_rpc = os.environ["POLYGON_CHAIN_JSON_RPC"]
-    launch = fork_network(mainnet_rpc, unlocked_addresses=[aDai_holder]) #"@34571963"
+    launch = fork_network(mainnet_rpc + "@34581439", unlocked_addresses=[aave_token_holder])
     yield launch.json_rpc_url
     # Wind down Ganache process after the test is complete
     launch.close()
@@ -57,11 +71,8 @@ def web3(ganache_polygon_chain_fork: str):
     return Web3(HTTPProvider(ganache_polygon_chain_fork))
 
 
-def test_get_deposit_balance(web3: Web3, aDai_holder: HexAddress):
+def test_get_deposit_balance(web3: Web3, aave_token: AaveToken, aave_token_holder: HexAddress):
 
-    # polygon aDAI: 0x82E64f49Ed5EC1bC6e43DAD4FC8Af9bb3A2312EE
-    aDai = HexAddress(HexStr("0x82E64f49Ed5EC1bC6e43DAD4FC8Af9bb3A2312EE"))
-    balance = aave_v3_get_deposit_balance(web3, aDai, aDai_holder)
-    print("balance:", balance)
-    assert balance > 0, "账户中aDai应大于0"
+    balance = aave_v3_get_deposit_balance(web3, aave_token.deposit_address, aave_token_holder)
+    assert balance == 229496.483194811632175501, "账户中AAVE DAI应等于229496.483194811632175501"
 
---
+++ tests/manual_polygon_example.py
@@ -0,0 +1,45 @@
+"""A sample script to manually operate aave on polygon.
+
+"""
+
+from web3 import Web3, HTTPProvider
+from eth_defi.aave_v3.balances import aave_v3_get_deposit_balance, aave_v3_get_variable_borrow_balance, aave_v3_get_stable_borrow_balance
+from eth_defi.aave_v3.constants import AaveNetwork, AaveToken, aave_v3_get_network_by_chain_id
+
+node = "https://polygon-mainnet.g.alchemy.com/v2/H3E99oH7XymptRueBxkO4OXXazhFb7_c"
+
+
+web3 = Web3(HTTPProvider(node))
+print(f"{node} current block is {web3.eth.block_number:,}")
+
+aave_network = aave_v3_get_network_by_chain_id(137)
+aave_token = aave_network.token_contracts['DAI']
+
+address_does_not_exist = Web3.toChecksumAddress("0x6564b5053C381a8D840B40d78bA229e2d8e912ed")
+
+# ValueError: {'code': -32000, 'message': 'exceed maximum block range: 5000'}
+balances = aave_v3_get_deposit_balance(web3,  aave_token.deposit_address, address_does_not_exist)
+print("Empty", balances)
+
+# https://polygonscan.com/address/0x5b8effbdae5941b938c1a79616d185cc3c79d4ff
+some_large_holder = Web3.toChecksumAddress("0x5b8effbdae5941b938c1a79616d185cc3c79d4ff")
+balances = aave_v3_get_deposit_balance(web3, aave_token.deposit_address, some_large_holder)
+print("Large ADAI holder", balances)
+
+# https://polygonscan.com/address/0x2f74bd41940a188eb8d0fd0d5a6e92e93ec7a4a8
+some_large_holder = Web3.toChecksumAddress("0x2f74bd41940a188eb8d0fd0d5a6e92e93ec7a4a8")
+aave_token = aave_network.token_contracts['DAI']
+balances = aave_v3_get_stable_borrow_balance(web3, Web3.toChecksumAddress(aave_token.stable_borrow_address), some_large_holder)
+print("Large stableDebtPolDAI holder", balances)
+
+some_large_holder = Web3.toChecksumAddress("0x5b8effbdae5941b938c1a79616d185cc3c79d4ff")
+aave_token = aave_network.token_contracts['EURS']
+balances = aave_v3_get_variable_borrow_balance(web3, Web3.toChecksumAddress(aave_token.variable_borrow_address), some_large_holder)
+print("Large variableDebtPolEURS holder", balances)
+
+# https://polygonscan.com/address/0xd1802c153fa2ae751ac7526419c4500fa8025c1e
+some_large_holder = Web3.toChecksumAddress("0xd1802c153fa2ae751ac7526419c4500fa8025c1e")
+aave_token = aave_network.token_contracts['EURS']
+balances = aave_v3_get_stable_borrow_balance(web3, Web3.toChecksumAddress(aave_token.stable_borrow_address), some_large_holder)
+print("Large stableDebtPolEURS holder", balances)
+
