9e1cdaee9e5d897ca72ee452eae0211b77984ab6
michael <michael@sun8am.com>
2022-11-09 23:51:35 +0800

    Update manual_venus_data.py

--- tests/venus/manual_venus_data.py
+++ tests/venus/manual_venus_data.py
@@ -10,7 +10,7 @@ json_rpc_url = os.environ["BNB_CHAIN_JSON_RPC"]
 # start_block = 22629320
 # end_block = 22639320   #2286905201  #
 
-start_block = 22837829 - 864000
-end_block = 22837829   #2286905201
+start_block = 22837829 - 864000 * 3
+end_block = 22837829 - 864000 * 2 #2286905201
 
-fetch_events_to_csv(json_rpc_url, JSONFileScanState("venus_events_state.log"), start_block=start_block, end_block=end_block, output_folder=".")
+fetch_events_to_csv(json_rpc_url, JSONFileScanState("venus_events_state.log"), max_workers=5, start_block=start_block, end_block=end_block, output_folder=".")
